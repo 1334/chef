41072752b96813813d901be5b9d23415d136c96d
1334 <1334@shadowproject.net>
2012-11-23 19:34:30 +0100

    fi initial_setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,8 +3,8 @@ apt-get -y update
 apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev curl git-core python-software-properties vim
 cd /tmp
 wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
-tar -xvzf ruby-1.9.3-p125.tar.gz
-cd ruby-1.9.3-p125/
+tar -xvzf ruby-1.9.3-p327.tar.gz
+cd ruby-1.9.3-p327/
 ./configure --prefix=/usr/local
 make
 make install
